98e6421979a95b3e93d637ce2fcefedaed215e87
Sebastián Jiménez <sebasj14@gmail.com>
2015-06-30 14:55:56 -0500

    Camino labrado para resetPass

--- CRUV1.0/server/config.js
+++ CRUV1.0/server/config.js
@@ -4,7 +4,7 @@ module.exports = {
 	mailer: {
 		auth: {
 			user: 'jcsudea@gmail.com',
-			pass: 'jcsudea123'
+			pass: '*****' //Ustedes saben cual es
 		},
 		defaultFromAddress: 'Gamification App ✔ <jcsudea@gmail.com>'
 	}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # proyectoIntegrador
 # por: Carolina Isaza, Sebastián Jiménez, Jaime Londoño
+Recuerden modificar el config.js con el password que es
