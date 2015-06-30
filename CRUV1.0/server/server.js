277d14fe19694348f453a2a1c88f3ec587de2480
Sebastián Jiménez <sebasj14@gmail.com>
2015-06-30 09:20:51 -0500

    Agregado nodemailer (modulo para correos

--- CRUV1.0/server/server.js
+++ CRUV1.0/server/server.js
@@ -3,6 +3,7 @@ var mongojs     =   require('mongojs');
 var morgan      =   require('morgan');
 var db          =   mongojs('mongodb://root:root@ds061391.mongolab.com:61391/integrador', ['usuarios', 'Retos', 'Problemas', 'Objetivos']);
 var server      =   restify.createServer();
+var nodemailer 	=	require('nodemailer');
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -24,3 +25,27 @@ server.listen(process.env.PORT || 9804, function () {
 //var manageUsers = require('./auth/manageUser');
 
 var manageUsers = require('./auth/manageUser')(server, db);
+
+var transporter = nodemailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: 'jcsudea@gmail.com',
+		pass: 'jcsudea123'
+	}
+});
+
+var mailOptions = {
+    from: 'Gamification App ✔ <jcsudea@gmail.com>', // sender address
+    to: 'sebasj14@gmail.com, carito9994@gmail.com, jaime951@gmail.com', // list of receivers
+    subject: 'Pruebas ✔', // Subject line
+    text: 'Hola, esto es una prueba del envío de correos con Nodemailer ✔', // plaintext body
+    html: '' // html body
+};
+/*
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    }
+});*/
